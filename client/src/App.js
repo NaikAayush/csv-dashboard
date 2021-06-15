1dc8b014208a6258e6a23d4be5ef9daed7d6e9c1
NaikAayush <ayush@pesu.pes.edu>
2021-06-15 19:20:46 +0530

    react fix, and setup express

---
+++ client/src/App.js
@@ -0,0 +1,116 @@
+import "./index.css";
+import "./App.css";
+
+export default function App() {
+  const data = [
+    {
+      id: 1,
+      name: "Peter",
+      sex: "Male",
+    },
+    {
+      id: 2,
+      name: "Alice",
+      sex: "Female",
+    },
+  ];
+  return (
+    <div className="flex flex-col items-center w-screen">
+      <NavBar> </NavBar>
+      <span className="mt-10"></span>
+      <Table1 className="w-1/2"></Table1>
+      {/* <Table list={data} pageCount={10} headers={("id", "name", "sex")} /> */}
+    </div>
+  );
+}
+
+function NavBar() {
+  return (
+    <nav className="bg-white shadow w-full">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="relative flex justify-between h-16">
+          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex-shrink-0 flex items-center">
+              <h1 className="text-lg leading-6 font-medium text-gray-900">
+                CSV Table
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+const people = [
+  {
+    name: "Jane Cooper",
+    title: "Regional Paradigm Technician",
+    role: "Admin",
+    email: "jane.cooper@example.com",
+  },
+];
+
+function Table1() {
+  return (
+    <div className="flex flex-col w-1/2">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Title
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Email
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Role
+                  </th>
+                  {/* <th scope="col" className="relative px-6 py-3">
+                    <span className="sr-only">Edit</span>
+                  </th> */}
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {people.map((person) => (
+                  <tr key={person.email}>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                      {person.name}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      {person.title}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      {person.email}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      {person.role}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
